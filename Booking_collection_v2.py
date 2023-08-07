abc0f39223b25ea522927eebe2a8fb7aeb13f231
walter0b <jakinjoram5@outlook.com>
2023-08-07 11:57:27 +0100

    update comments

--- Booking_collection_v2.py
+++ Booking_collection_v2.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# Set the URL you want to open
+# Set the URL To the Peg engine
 url =  input("Press Enter the URL to the peg engine.")
 
 # Defult peg engine url
